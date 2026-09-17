2200ac730d5dbfe9ff8c3b07d7b53be7b84867c2
satokhernoel-maker <sato.khernoel@cec.edu.ph>
2026-09-17 15:29:16 +0800

    Create new supabase.js

---
+++ new supabase.js
@@ -0,0 +1,8 @@
+
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = https://kjofyfntpvfujophbdla.supabase.co;
+const supabaseKey = "sb_secret_wOEWHe01eGXxrkLj451wtw_n5u2dV2T";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
